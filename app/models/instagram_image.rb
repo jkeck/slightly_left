fe27ce371f6160c69e4d9788dc49d171b9ff1529
Jessie Keck <jessie.keck@gmail.com>
2013-07-24 23:12:06 -0700

    Object-ify InstagramImage class a bit more.

--- app/models/instagram_image.rb
+++ app/models/instagram_image.rb
@@ -1,48 +1,75 @@
 require "net/http"
 class InstagramImage
+  attr_accessor :tag
+  def initialize(tag)
+    @tag = tag
+  end
+  
   def self.find tag
-    if self.refresh_from_instagram?
-      data = Net::HTTP.get self.instagram_url(tag)
-      json = JSON.parse data
-      return [] unless json["data"]
-      selected_images = json["data"].select do |data|
-        self.known_user_ids.include? data["user"]["id"]
-      end
-      unless selected_images.blank?
-        self.instagram_images_from_database.map{|image| image.destroy }
-        selected_images.map do |data|
-          img = Image.new
-          img.thumbnail_url = data["images"]["thumbnail"]["url"]
-          img.full_url = data["images"]["standard_resolution"]["url"]
-          img.caption = data["caption"]["text"]
-          img.source = "Instagram"
-          img.save
-          img
+    InstagramImage.new(tag).find
+  end
+  
+  def find
+    if refresh_from_instagram?
+      unless json_from_instagram["data"] and images_from_allowed_users.blank?
+        InstagramImage.destroy_all
+        images = selected_images
+        if json_from_instagram["pagination"] and json_from_instagram["pagination"]["next_url"]
+          next_page = json_from_instagram(json_from_instagram["pagination"]["next_url"])
+          images << selected_images(images_from_allowed_users(next_page))
         end
+        images.flatten
       else
-        self.instagram_images_from_database
+        instagram_images_from_database
       end
     else
-      self.instagram_images_from_database
+      instagram_images_from_database
     end
   end
 
   private
-  def self.instagram_images_from_database
-    Image.where(source: "Instagram")
+  def selected_images images = images_from_allowed_users
+    images.map do |data|
+      img = Image.new
+      img.thumbnail_url = data["images"]["thumbnail"]["url"]
+      img.full_url = data["images"]["standard_resolution"]["url"]
+      img.caption = data["caption"]["text"]
+      img.source = InstagramImage.source
+      img.save
+      img
+    end
   end
-  def self.refresh_from_instagram?
-    return true if self.instagram_images_from_database.blank?
-    (self.instagram_images_from_database.first.created_at + 10.minutes) < Time.now.utc
+  def json_from_instagram url = instagram_url
+    JSON.parse Net::HTTP.get URI.parse(url)
   end
-  def self.instagram_url tag
-    URI.parse "https://api.instagram.com/v1/tags/#{tag}/media/recent?access_token=#{self.access_token}"
+  def images_from_allowed_users json = json_from_instagram
+    return [] unless json["data"]
+    json["data"].select do |data|
+      known_user_ids.include? data["user"]["id"]
+    end
   end
-  def self.access_token
+  def instagram_images_from_database
+    @instagram_images_from_database ||= Image.where(source: InstagramImage.source)
+  end
+  def refresh_from_instagram?
+    return true if instagram_images_from_database.blank?
+    (instagram_images_from_database.first.created_at + 10.minutes) < Time.now.utc
+  end
+  def instagram_url
+    "https://api.instagram.com/v1/tags/#{tag}/media/recent?access_token=#{access_token}"
+  end
+  def access_token
     SlightlyLeft::Application.config.instagram_access_token
   end
-  def self.known_user_ids
+  def known_user_ids
     SlightlyLeft::Application.config.allowed_instagram_ids
   end
-
+  def self.source
+    "Instagram"
+  end
+  def self.destroy_all
+    Image.where(source: InstagramImage.source).each do |image|
+      image.destroy
+    end
+  end
 end
